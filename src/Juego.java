4a6f625d93f0a74690c49165bec6f2ed1e2d16ae
Valle8 <72798162+Valle8@users.noreply.github.com>
2020-11-01 22:15:16 +0100

    completo

--- src/Juego.java
+++ src/Juego.java
@@ -5,7 +5,7 @@ public class Juego {
 	public static void main(String[] args) {
 		String resp;
 		int contadorpuntos=0;
-		String ent = JOptionPane.showInputDialog("Bienvenido al Trivial sobre los elementos de Pokemon. ¿Desea jugar?"
+		String ent = JOptionPane.showInputDialog("Bienvenido al Trivial sobre los elementos y movimientos de Pokemon. ¿Desea jugar?"
 				+ "\n 1.SI"
 				+ "\n 2.NO");
 		
@@ -44,29 +44,55 @@ public class Juego {
 				System.out.println("Incorrecto");
 				contadorpuntos = contadorpuntos - 1;
 			}
-			
-			
-	
-		  
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-
-			
-			
-			
-			
-			
-			
-			
-			
+			resp = JOptionPane.showInputDialog("¿Es débil el tipo hada frente al tipo siniestro ?");
+			if (resp.equals("no")) {
+				System.out.println("Correcto");
+				contadorpuntos = contadorpuntos + 1;
+			} else {
+				System.out.println("Incorrecto");
+				contadorpuntos = contadorpuntos - 1;
+			}
+			resp = JOptionPane.showInputDialog("¿Es eficaz el tipo normal frente al fantasma?");
+			if (resp.equals("no")) {
+				System.out.println("Correcto");
+				contadorpuntos = contadorpuntos + 1;
+			} else {
+				System.out.println("Incorrecto");
+				contadorpuntos = contadorpuntos - 1;
+			}
+			resp = JOptionPane.showInputDialog("¿De qué tipo es el movimiento Rayo Solar?");
+			if (resp.equals("planta")) {
+				System.out.println("Correcto");
+				contadorpuntos = contadorpuntos + 1;
+			} else {
+				System.out.println("Incorrecto");
+				contadorpuntos = contadorpuntos - 1;
+			}
+			resp = JOptionPane.showInputDialog("¿Cuántos elementos hay en Pokemon?");
+			if (resp.equals("18")) {
+				System.out.println("Correcto");
+				contadorpuntos = contadorpuntos + 1;
+			} else {
+				System.out.println("Incorrecto");
+				contadorpuntos = contadorpuntos - 1;
+			}
+			resp = JOptionPane.showInputDialog("¿Hiperrayo es de tipo Especial o Fisico?");
+			if (resp.equals("especial")) {
+				System.out.println("Correcto");
+				contadorpuntos = contadorpuntos + 1;
+			} else {
+				System.out.println("Incorrecto");
+				contadorpuntos = contadorpuntos - 1;
+			}
+			resp = JOptionPane.showInputDialog("¿De que tipo es el movimiento Abocajarro?");
+			if (resp.equals("lucha")) {
+				System.out.println("Correcto");
+				contadorpuntos = contadorpuntos + 1;
+			} else {
+				System.out.println("Incorrecto");
+				contadorpuntos = contadorpuntos - 1;
+			}
+			System.out.println("Tu puntuación es " + contadorpuntos + " de 10 ");
 		}
 
 	}
