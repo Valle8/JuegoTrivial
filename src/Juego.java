d36e688c2d2f88d2afea7531af933ccca44fc271
Valle8 <72798162+Valle8@users.noreply.github.com>
2020-11-01 22:04:17 +0100

    segunda

--- src/Juego.java
+++ src/Juego.java
@@ -3,7 +3,7 @@ import javax.swing.JOptionPane;
 public class Juego {
 
 	public static void main(String[] args) {
-		String resp1;
+		String resp;
 		int contadorpuntos=0;
 		String ent = JOptionPane.showInputDialog("Bienvenido al Trivial sobre los elementos de Pokemon. ¿Desea jugar?"
 				+ "\n 1.SI"
@@ -12,15 +12,22 @@ public class Juego {
 		int respuesta = Integer.parseInt(ent);
 		if (respuesta == 1) {
 			System.out.println("Por favor responda con minúsculas");
-			resp1 = JOptionPane.showInputDialog("¿Es débil el tipo planta frente al tipo fuego?");
-			if (resp1.equals("si")) {
+			resp = JOptionPane.showInputDialog("¿Es débil el tipo planta frente al tipo fuego?");
+			if (resp.equals("si")) {
 				System.out.println("Correcto");
 				contadorpuntos = contadorpuntos + 1;
 			} else {
 				System.out.println("Incorrecto");
 				contadorpuntos = contadorpuntos - 1;
 			}
-			
+			resp = JOptionPane.showInputDialog("¿Es débil el tipo acero frente al tipo lucha ?");
+			if (resp.equals("si")) {
+				System.out.println("Correcto");
+				contadorpuntos = contadorpuntos + 1;
+			} else {
+				System.out.println("Incorrecto");
+				contadorpuntos = contadorpuntos - 1;
+			}
 	
 		  
 			
